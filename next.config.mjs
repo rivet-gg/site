1b68b566f3ccfce29d3a3f5a6c288ae3b7bad792
Nathan Flurry <developer@nathanflurry.com>
2023-06-24 04:02:11 -0700

    Update output mode

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,7 @@ const withMDX = nextMDX({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   experimental: {
