67ac3e308598f18ea480dde139090296c6793f5e
Nathan Flurry <developer@nathanflurry.com>
2023-06-23 21:30:19 -0700

    FIx blog

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,11 @@ import nextMDX from '@next/mdx';
 import withSearch from './src/mdx/search.mjs';
 import { remarkPlugins } from './src/mdx/remark.mjs';
 import { rehypePlugins } from './src/mdx/rehype.mjs';
-import { recmaPlugins } from './src/mdx/recma.mjs';
 
 const withMDX = nextMDX({
   options: {
     remarkPlugins,
     rehypePlugins,
-    recmaPlugins,
     providerImportSource: '@mdx-js/react'
   }
 });
