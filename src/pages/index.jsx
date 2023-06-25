7ed06e206d52701d6a468ff094be9102d9457bfb
Nathan Flurry <developer@nathanflurry.com>
2023-06-25 16:55:07 -0700

    Add feature pagination on home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import { generateRssFeed } from '@/lib/generateRssFeed';
 import { getAllArticles } from '@/lib/getAllArticles';
 import { HeroPattern } from '@/components/HeroPattern';
 import clsx from 'clsx';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const tabs = [
   { name: 'Game Servers' },
@@ -67,23 +68,63 @@ function Title() {
   );
 }
 
+const variants = {
+  enter: direction => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1
+  },
+  exit: direction => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0
+    };
+  }
+};
+
+/**
+ * Experimenting with distilling swipe offset and velocity into a single variable, so the
+ * less distance a user has swiped, the more velocity they need to register as a swipe.
+ * Should accomodate longer swipes and short flicks without having binary checks on
+ * just distance thresholds and velocity > 0.
+ */
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset, velocity) => {
+  return Math.abs(offset) * velocity;
+};
+
+function paginate(page, dir) {
+  const newPage = page + dir;
+  if (newPage < 0) return { index: tabs.length - (-newPage % tabs.length), dir };
+  return { index: newPage % tabs.length, dir };
+}
+
 function Features() {
-  const [tabIndex, setTabIndex] = React.useState(0);
+  const [page, setPage] = React.useState({ index: 0, dir: 1 });
 
   return (
-    <div className='max-w-64 ring-inse mt-16 h-[500px] w-full rounded-md shadow-2xl ring-1 ring-gray-800/10 dark:ring-gray-200/10 sm:mt-24'>
-      <Tabs tabIndex={tabIndex} onChangeTabIndex={setTabIndex} />
+    <div className='ring-inse mt-16 h-[500px] w-[1000px] rounded-md shadow-2xl ring-1 ring-gray-800/10 dark:ring-gray-200/10 sm:mt-24'>
+      <Tabs index={page.index} onChangeTab={i => setPage({ index: i, dir: i > page.index ? 1 : -1 })} />
+
+      <Pages page={page} onChangePage={setPage} />
     </div>
   );
 }
 
-function Tabs({ tabIndex, onChangeTabIndex }) {
+function Tabs({ index, onChangeTab }) {
   return (
     <div className='hidden sm:block'>
       <div className='border-b border-gray-200'>
         <nav className='-mb-px flex' aria-label='Tabs'>
           {tabs.map((tab, i) => {
-            let isCurrent = i == tabIndex;
+            let isCurrent = i == index;
             return (
               <a
                 key={tab.name}
@@ -95,7 +136,7 @@ function Tabs({ tabIndex, onChangeTabIndex }) {
                   'w-1/4 cursor-pointer border-b-2 px-1 py-4 text-center text-sm font-medium'
                 )}
                 aria-current={isCurrent ? 'page' : undefined}
-                onClick={() => onChangeTabIndex(i)}>
+                onClick={() => onChangeTab(i)}>
                 {tab.name}
               </a>
             );
@@ -105,5 +146,48 @@ function Tabs({ tabIndex, onChangeTabIndex }) {
     </div>
   );
 }
+
+function Pages({ page, onChangePage }) {
+  return (
+    <div className='relative flex h-72 w-full items-center justify-center'>
+      <AnimatePresence initial={false} custom={page.dir}>
+        <motion.div
+          key={page.index}
+          className='absolute h-full w-full bg-red-500'
+          custom={page.dir}
+          variants={variants}
+          initial='enter'
+          animate='center'
+          exit='exit'
+          transition={{
+            x: { type: 'spring', stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 }
+          }}
+          drag='x'
+          dragConstraints={{ left: 0, right: 0 }}
+          dragElastic={1}
+          onDragEnd={(e, { offset, velocity }) => {
+            const swipe = swipePower(offset.x, velocity.x);
+
+            if (swipe < -swipeConfidenceThreshold) {
+              onChangePage(paginate(page.index, 1, ));
+            } else if (swipe > swipeConfidenceThreshold) {
+              onChangePage(paginate(page.index, -1));
+            }
+          }}>
+          Hello page {page.index}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
+}
+
+// function Page({ index, children }) {
+//   return (
+//     <div className='absolute h-full w-full bg-red-500' style={{ left: `${index * 100}%` }}>
+//       {children}
+//     </div>
+//   );
+// }
 
 Index.prose = false;
