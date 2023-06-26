a2b2f6ed32e992ba189ce62c508f1fc9615efffe
Nathan Flurry <developer@nathanflurry.com>
2023-06-26 00:15:27 -0700

    WIP launch CTA

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,6 +12,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faBolt,
   faBook,
+  faGraduationCap,
+  faBooks,
+  faChessKnight,
+  faGlobe,
   faChartWaterfall,
   faClockRotateLeft,
   faComment,
@@ -224,6 +228,8 @@ export default function Index() {
             <Features />
             <CaseStudies />
           </div>
+
+          <UpAndRunning />
         </div>
       </div>
     </div>
@@ -462,7 +468,7 @@ function PageGameServers({ page }) {
 
 function CaseStudies({ props }) {
   return (
-    <div className='mt-16'>
+    <div className='mt-40'>
       {/* Title */}
       <div className='mx-auto max-w-2xl text-center'>
         <h2 className='text-xl font-bold tracking-tight text-white sm:text-2xl'>
@@ -471,7 +477,7 @@ function CaseStudies({ props }) {
       </div>
 
       {/* Grid */}
-      <div className='-mx-6 grid grid-cols-2 gap-0.5 overflow-hidden sm:mx-0 sm:rounded-2xl md:grid-cols-3 mt-6'>
+      <div className='-mx-6 mt-6 grid grid-cols-2 gap-0.5 overflow-hidden sm:mx-0 sm:rounded-2xl md:grid-cols-3'>
         <div className='bg-white/5 p-8 sm:p-10'>
           <img
             className='max-h-12 w-full object-contain'
@@ -526,6 +532,67 @@ function CaseStudies({ props }) {
             height={48}
           />
         </div>
+      </div>
+    </div>
+  );
+}
+
+import { CheckCircleIcon } from '@heroicons/react/20/solid';
+import { Resource, ResourceGroup } from '@/components/Resources';
+
+const benefits = [
+  'Competitive salaries',
+  'Flexible work hours',
+  '30 days of paid vacation',
+  'Annual team retreats',
+  'Benefits for you and your family',
+  'A great work environment'
+];
+
+function UpAndRunning() {
+  return (
+    <div className='relative isolate mt-40'>
+      <div className='mx-auto max-w-7xl sm:px-6 lg:px-8'>
+        <div className='mx-auto flex max-w-2xl flex-col gap-16 bg-white/5 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20'>
+          <img
+            className='h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm'
+            src='https://images.unsplash.com/photo-1519338381761-c7523edc1f46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80'
+            alt=''
+          />
+          <div className='w-full flex-auto'>
+            <h2 className='text-3xl font-bold tracking-tight text-white sm:text-4xl'>
+              Launch your game in <span className='text-purple-300'>minutes</span>
+            </h2>
+            <p className='mt-6 text-lg leading-8 text-gray-300'>
+              Just 3 lines of code and you're off to the races.
+            </p>
+
+            <div className='mt-4 grid grid-cols-1 gap-8 border-t border-zinc-900/5 pt-10 dark:border-white/5 sm:grid-cols-2 xl:grid-cols-3'>
+              <Resource title='Getting started' icon={faGraduationCap} href='/general/guides/crash-course' />
+              <Resource title='Tutorials' icon={faBooks} href='/general/libraries' />
+              <Resource
+                title='Video tutorials'
+                icon={faServer}
+                iconType='duotone'
+                href='/serverless-lobbies/introduction'
+              />
+              <Resource title='API' icon={faGlobe} href='/cdn/introduction' />
+              <Resource title='GitHub' icon={faGlobe} href='/cdn/introduction' />
+              <Resource title='Community' icon={faUserGroup} href='/cdn/introduction' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className='absolute inset-x-0 -top-16 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl'
+        aria-hidden='true'>
+        {/* <div
+          className='aspect-[1318/752] w-[82.375rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-25'
+          style={{
+            clipPath:
+              'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)'
+          }}
+        /> */}
       </div>
     </div>
   );
