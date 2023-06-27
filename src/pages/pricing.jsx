a5d3a853c42f4c7ebc741b9f9c7e2d22da7106f1
Nathan Flurry <developer@nathanflurry.com>
2023-06-27 03:06:55 -0700

    Tweak pricing

--- src/pages/pricing.jsx
+++ src/pages/pricing.jsx
@@ -4,84 +4,6 @@ import { Button } from '@/components/Button';
 import { CheckIcon, MinusIcon } from '@heroicons/react/20/solid';
 import clsx from 'clsx';
 
-// const pricing = {
-//   frequencies: [
-//     { value: 'monthly', label: 'Monthly' },
-//     { value: 'annually', label: 'Annually' }
-//   ],
-//   tiers: [
-//     {
-//       name: 'Open Source',
-//       id: 'tier-starter',
-//       buttonName: 'View on GitHub',
-//       href: 'https://github.com/rivet-gg/rivet',
-//       featured: false,
-//       description: 'All your essential business finances, taken care of.',
-//       mainFeatures: [
-//         'Self-host on your hardware',
-//         'Read & modify source code',
-//         'Permissive license (Apache 2.0)',
-//       ]
-//     },
-//     {
-//       name: 'Cloud',
-//       id: 'tier-scale',
-//       buttonName: 'Sign Up',
-//       href: '#',
-//       featured: true,
-//       description: 'The best financial services for your thriving business.',
-//       mainFeatures: [
-//         'Auto-scaling game servers',
-//         'DDoS mitigation',
-//         'Flexible matchmaking',
-//         'Streamlined team collaboration',
-//         'No downtime deploys',
-//         'CDN for hosting assets & webpages'
-//       ]
-//     },
-//     {
-//       name: 'Enterprise',
-//       id: 'tier-growth',
-//       buttonName: 'Contact Us',
-//       href: '#',
-//       featured: false,
-//       description: 'Convenient features to take your business to the next level.',
-//       mainFeatures: ['Host on-premise', 'SLA', 'Support']
-//     }
-//   ],
-//   sections: [
-//     {
-//       name: 'Services',
-//       features: [
-//         { name: 'Analytics', tiers: [0, 1, 1] },
-//         { name: 'Audit log', tiers: [0, 0, 1] },
-//         { name: 'SSO', tiers: [0, 0, 1] },
-//         {
-//           name: 'Cloud providers (more coming soon)',
-//           tiers: ['Linode', 'Linode', 'On-premise, AWS, Linode']
-//         },
-//         { name: 'Bring-your-own-server', tiers: ['Coming soon', 'Coming soon', 1] }
-//       ]
-//     },
-//     {
-//       name: 'Cluster',
-//       features: [
-//         { name: 'On-premise deployment', tiers: [1, 0, 1] },
-//         { name: 'Autoscaling (cost saving)', tiers: [0, 1, 1] },
-//         { name: 'High availability', tiers: [0, 1, 1] },
-//         { name: 'Horzontal scaling', tiers: [0, 1, 1] }
-//       ]
-//     },
-//     {
-//       name: 'Support',
-//       features: [
-//         { name: 'Support', tiers: ['Community', 'Standard', 'Business or Enterprise'] },
-//         { name: 'Onboarding', tiers: ['Community', 'Community', 1] }
-//       ]
-//     }
-//   ]
-// };
-
 export default function Pricing() {
   const [open, setOpen] = useState(true);
 
@@ -114,28 +36,42 @@ export default function Pricing() {
   ];
   const sections = [
     {
-      name: '',
+      name: 'Pricing',
       features: [
         {
-          name: 'Price',
+          name: 'CCU & MAU',
           tiers: {
-            'tier-oss': 'Free',
-            'tier-cloud': (
-              <Button variant='text' arrow='right' onClick={() => setOpen(true)}>
-                Pricing Calculator
-              </Button>
-            ),
-            'tier-enterprise': 'Custom'
+            'tier-oss': 'Unlimited',
+            'tier-cloud': 'Unlimited',
+            'tier-enterprise': 'Unlimited'
           }
         },
         {
-          name: 'Freebies',
+          name: 'Serverless Lobbies',
           tiers: {
+            'tier-oss': 'Self-hosted',
             'tier-cloud': (
-              <div className='mx-auto w-max gap-0.5 rounded-full bg-violet-400/10 px-3 py-1 text-sm font-semibold text-violet-400 ring-1 ring-inset ring-violet-400/20 transition'>
-                1 free server
-              </div>
-            )
+              <Button variant='text' arrow='right' onClick={() => setOpen(true)}>
+                Calculator
+              </Button>
+            ),
+            'tier-enterprise': 'Contact Us'
+          }
+        },
+        {
+          name: 'Custom Domain with SSL',
+          tiers: {
+            'tier-oss': 'Self-hosted',
+            'tier-cloud': '$5/month/domain',
+            'tier-enterprise': 'Contact Us'
+          }
+        },
+        {
+          name: 'CDN',
+          tiers: {
+            'tier-oss': 'Self-hosted',
+            'tier-cloud': '$0.05/GB/month (1 TB free)',
+            'tier-enterprise': 'Contact Us'
           }
         }
       ]
@@ -143,9 +79,13 @@ export default function Pricing() {
     {
       name: 'Features',
       features: [
+        {
+          name: 'Game Guard (DDoS + SSL)',
+          tiers: { 'tier-oss': 'Limited', 'tier-cloud': true, 'tier-enterprise': true }
+        },
         { name: 'Analytics', tiers: { 'tier-cloud': true, 'tier-enterprise': true } },
         { name: 'Audit log', tiers: { 'tier-enterprise': true } },
-        { name: 'SSO', tiers: { 'tier-enterprise': true } }
+        { name: 'Single Sign-On (SAML 2.0)', tiers: { 'tier-enterprise': true } }
       ]
     },
     {
@@ -176,7 +116,7 @@ export default function Pricing() {
       features: [
         {
           name: 'Support',
-          tiers: { 'tier-oss': 'Community', 'tier-cloud': 'Standard', 'tier-enterprise': 'Custom' }
+          tiers: { 'tier-oss': 'Community', 'tier-cloud': 'Standard', 'tier-enterprise': 'Contact Us' }
         }
       ]
     }
@@ -379,28 +319,23 @@ function PricingModal({ open, onClose }) {
               leave='ease-in duration-200'
               leaveFrom='opacity-100 translate-y-0 sm:scale-100'
               leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'>
-              <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6'>
-                <div>
-                  <div className='mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100'>
-                    <CheckIcon className='h-6 w-6 text-green-600' aria-hidden='true' />
-                  </div>
-                  <div className='mt-3 text-center sm:mt-5'>
-                    <Dialog.Title as='h3' className='text-base font-semibold leading-6 text-gray-900'>
-                      Payment successful
-                    </Dialog.Title>
-                    <div className='mt-2'>
-                      <p className='text-sm text-gray-500'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet labore.
-                      </p>
-                    </div>
-                  </div>
+              <Dialog.Panel className='relative transform overflow-hidden rounded-xl bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6'>
+                {/* Content */}
+                <div className='mt-1 text-center'>
+                  <Dialog.Title as='h3' className='text-3xl font-bold text-gray-900'>
+                    Cloud Pricing
+                  </Dialog.Title>
+
+                  <div className='mt-2'>Pricing</div>
                 </div>
+
+                {/* Buttons */}
                 <div className='mt-5 sm:mt-6'>
                   <button
                     type='button'
                     className='inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
                     onClick={onClose}>
-                    Go back to dashboard
+                    Close
                   </button>
                 </div>
               </Dialog.Panel>
