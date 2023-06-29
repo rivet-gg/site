1e1b0c006b759e6517415a3080d9ce0fdc878256
Nathan Flurry <developer@nathanflurry.com>
2023-06-29 16:33:34 -0700

    Fix image imports

--- scripts/checkBrokenLinks.sh
+++ scripts/checkBrokenLinks.sh
@@ -4,4 +4,4 @@ set -euf -o pipefail
 # docker run --init -it -v `pwd`:/input lycheeverse/lychee --config /input/lychee.toml --base /input/src/pages '/input/src/pages/**/*.mdx'
 # muffet --verbose --max-connections=16 --exclude='(hub.rivet.gg|tanks.rivet.game|discord.gg|twitter.com|github.com|cdn-cgi)' https://staging.rivet.gg
 
-npx linkinator --config linkinator.config.json https://c4594a48.site-ckd.pages.dev/
+npx linkinator --config linkinator.config.json https://85ac9664.site-ckd.pages.dev/
