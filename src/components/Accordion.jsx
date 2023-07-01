48035cfde853889ebccdb791fa6d77172ab8b4ad
Nathan Flurry <developer@nathanflurry.com>
2023-07-01 15:53:04 -0700

    Fix accordion styling

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,3 +1,5 @@
+import { faCaretDown, faCaretRight } from '@fortawesome/pro-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { clsx } from 'clsx';
 import React, { ReactNode, useState } from 'react';
 
@@ -15,7 +17,7 @@ function getAccordionStyleFromVariant(variant) {
   // Rounding is handled in Accordion by passing in isRounded to AccordionCover.
   return {
     parentClass: 'border dark:border-zinc-800 rounded-xl mb-3 overflow-hidden',
-    coverClass: 'py-4 px-5 space-x-2 hover:bg-slate-100 hover:dark:bg-slate-800 rounded-t-xl',
+    coverClass: 'py-4 px-5 space-x-2 hover:bg-violet-100 hover:dark:bg-white/10 rounded-t-xl transition',
     contentClass: 'mt-2 mb-4 mx-6'
   };
 }
@@ -62,7 +64,7 @@ export function AccordionGroup({ children }) {
   // when inside of an Accordion group.
   return (
     <div
-      className='prose-slate dark:prose-dark prose mb-3 mt-0 divide-y divide-inherit overflow-hidden rounded-xl border dark:border-zinc-800 [&>div>button]:rounded-none [&>div]:mb-0 [&>div]:rounded-none [&>div]:border-0'
+      className='mb-3 mt-0 divide-y divide-inherit overflow-hidden rounded-xl border dark:border-zinc-800 [&>div>button]:rounded-none [&>div]:mb-0 [&>div]:rounded-none [&>div]:border-0'
       role='list'>
       {children}
     </div>
@@ -79,7 +81,7 @@ function AccordionCover({ title, description, open, setOpen, icon, coverClass })
       aria-controls={title + 'Children'}
       aria-expanded={open}>
       <div className='mr-0.5'>
-        <ExpandableItemCoverIcon open={open} />
+        <FontAwesomeIcon icon={open ? faCaretDown : faCaretRight} className='w-3 h-3 text-slate-800 dark:text-slate-100 opacity-75' />
       </div>
       {icon ? (
         <div className='h-4 w-4 fill-slate-800 text-slate-800 dark:fill-slate-100 dark:text-slate-100'>
@@ -91,37 +93,5 @@ function AccordionCover({ title, description, open, setOpen, icon, coverClass })
         {description ? <p className='m-0 text-slate-900 dark:text-slate-200'>{description}</p> : null}
       </div>
     </button>
-  );
-}
-
-function ExpandableCover({ title, description, open, setOpen }) {
-  return (
-    <button
-      onClick={() => setOpen(!open)}
-      className={`not-prose mt-1.5 flex w-full flex-row content-center items-center py-1 text-slate-700 hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-200 ${
-        !open ? 'rounded-b-xl' : ''
-      }`}
-      aria-controls={title + 'Children'}
-      aria-expanded={open}>
-      <ExpandableItemCoverIcon open={open} />
-      <div className='ml-2 text-left leading-tight'>
-        <p className='m-0'>
-          {open ? 'Hide' : 'Show'} {title}
-        </p>
-        {description ? <p className='m-0'>{description}</p> : null}
-      </div>
-    </button>
-  );
-}
-
-function ExpandableItemCoverIcon({ open }) {
-  return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className={clsx('h-3 w-3 transition', open && '-mt-1 rotate-90 duration-200', !open && 'duration-75')}
-      viewBox='0 0 256 512'
-      fill='currentColor'>
-      <path d='M118.6 105.4l128 127.1C252.9 239.6 256 247.8 256 255.1s-3.125 16.38-9.375 22.63l-128 127.1c-9.156 9.156-22.91 11.9-34.88 6.943S64 396.9 64 383.1V128c0-12.94 7.781-24.62 19.75-29.58S109.5 96.23 118.6 105.4z' />
-    </svg>
   );
 }
