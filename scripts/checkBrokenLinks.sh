90c38d9c3ecb1cb8d5296b55115d2e3c0e01223a
Nathan Flurry <developer@nathanflurry.com>
2023-06-30 17:09:57 -0700

    Add redirect for docs

--- scripts/checkBrokenLinks.sh
+++ scripts/checkBrokenLinks.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -euf -o pipefail
 
-URL=https://bd34e083.site-ckd.pages.dev/
+URL=https://abb96f43.site-ckd.pages.dev/
 
 # docker run --init -it -v `pwd`:/input lycheeverse/lychee --config /input/lychee.toml --base /input/src/pages '/input/src/pages/**/*.mdx'
 # muffet --verbose --max-connections=16 --exclude='(hub.rivet.gg|tanks.rivet.game|discord.gg|twitter.com|github.com|cdn-cgi)' https://staging.rivet.gg
