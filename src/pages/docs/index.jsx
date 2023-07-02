2b849bbe3ca052e6e0ba3f8cda267c2f07e6bb9a
Nathan Flurry <developer@nathanflurry.com>
2023-07-02 10:43:04 -0700

    Add automatic redirect for docs

--- src/pages/docs/index.jsx
+++ src/pages/docs/index.jsx
@@ -1,5 +1,13 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Docs() {
+  const router = useRouter();
+
+  // Make sure we're in the browser
+  if (typeof window !== 'undefined') {
+    router.push('/docs/general');
+  }
+
   return <Link href='/docs/general'>Redirecting</Link>;
 }
