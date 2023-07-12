ce0eadbf092aea4e2648d3b39c693bc4ce5bb71e
Nathan Flurry <developer@nathanflurry.com>
2023-07-12 11:07:03 -0700

    Update pricing

--- src/pages/pricing.jsx
+++ src/pages/pricing.jsx
@@ -17,6 +17,37 @@ import imgGameGuard from '@/images/products/game-guard-duotone.svg';
 import imgAnalytics from '@/images/products/analytics-duotone.svg';
 import { faInfinity } from '@fortawesome/pro-solid-svg-icons';
 
+// Target on-demand price: ~$22/mo
+// Hathora runs c6a.2xlarge, ~$27.54
+const ON_DEMAND_PRICE = 2200;
+// For reference, AWS saving over on-demand: ~37%
+// Our savings over on demand: 30%
+// This factors in the amount of compute we assume clients will not use
+const RESERVED_SAVINGS = 0.3;
+// Our margin: 66%
+const MARGIN = 0.45;
+const MARKUP = 1 / (1 - MARGIN);
+// What % of the unused compute we can auto-scale: 40%
+// 100% means we never have unused compute
+const AUTOSCALING_FACTOR = 0.4;
+// const PRICE_PER_CORE = ON_DEMAND_PRICE * (1 - RESERVED_SAVINGS) * (1 - RESERVED_SAVINGS * AUTOSCALING_FACTOR) * MARKUP;
+const PRICE_PER_CORE = 2600;
+
+let UNIT_CORE = {
+  ram: 2000,
+  bandwidth: 1000
+};
+
+let TIERS = [
+  { name: '¹⁄₁₆', unit: 1 / 16 },
+  { name: '⅛', unit: 1 / 8 },
+  { name: '¼', unit: 1 / 4 },
+  { name: '½', unit: 1 / 2 },
+  { name: '1', unit: 1 },
+  { name: '2', unit: 2 },
+  { name: '4', unit: 4 }
+];
+
 export default function Pricing() {
   const [open, setOpen] = useState(false);
 
@@ -78,7 +109,7 @@ export default function Pricing() {
           icon: imgCdn,
           tiers: {
             'tier-oss': 'Self-hosted',
-            'tier-cloud': '$5/month/domain',
+            'tier-cloud': '$5/domain/month',
             'tier-enterprise': 'Contact Us'
           }
         },
@@ -190,8 +221,7 @@ export default function Pricing() {
               className={clsx(
                 tier.mostPopular ? 'rounded-xl bg-white/5 ring-1 ring-inset ring-white/10' : '',
                 'p-8'
-              )}
-            >
+              )}>
               <h2 className='mt-2 flex items-baseline gap-x-1 text-4xl font-bold text-white'>{tier.name}</h2>
               <a
                 href={tier.href}
@@ -201,8 +231,7 @@ export default function Pricing() {
                     ? 'bg-indigo-500 text-white hover:bg-indigo-400 focus-visible:outline-indigo-500'
                     : 'bg-white/10 text-white hover:bg-white/20 focus-visible:outline-white',
                   'mt-8 block rounded-md px-3 py-2 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2'
-                )}
-              >
+                )}>
                 {tier.button}
               </a>
               <ul role='list' className='mt-10 space-y-4 text-sm leading-6 text-white'>
@@ -241,8 +270,7 @@ export default function Pricing() {
                 <div
                   className='flex w-1/4 px-4'
                   aria-hidden='true'
-                  style={{ marginLeft: `${(tiers.findIndex(tier => tier.mostPopular) + 1) * 25}%` }}
-                >
+                  style={{ marginLeft: `${(tiers.findIndex(tier => tier.mostPopular) + 1) * 25}%` }}>
                   <div className='w-full rounded-t-xl border-x border-t border-white/10 bg-white/5' />
                 </div>
               </div>
@@ -278,8 +306,7 @@ export default function Pricing() {
                             ? 'bg-indigo-500 hover:bg-indigo-400 focus-visible:outline-indigo-600'
                             : 'bg-white/10 hover:bg-white/20 focus-visible:outline-white',
                           'block rounded-md px-3 py-2 text-center text-sm font-semibold leading-6 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2'
-                        )}
-                      >
+                        )}>
                         {tier.button}
                       </a>
                     </td>
@@ -294,8 +321,7 @@ export default function Pricing() {
                         className={clsx(
                           sectionIdx === 0 ? 'pt-8' : 'pt-16',
                           'pb-4 text-sm font-semibold leading-6 text-white'
-                        )}
-                      >
+                        )}>
                         {section.name}
                         <div className='absolute inset-x-8 mt-4 h-px bg-white/10' />
                       </th>
@@ -358,8 +384,7 @@ function PricingModal({ open, onClose }) {
           enterTo='opacity-100'
           leave='ease-in duration-200'
           leaveFrom='opacity-100'
-          leaveTo='opacity-0'
-        >
+          leaveTo='opacity-0'>
           <div className='fixed inset-0 bg-black bg-opacity-75 transition-opacity' />
         </Transition.Child>
 
@@ -372,8 +397,7 @@ function PricingModal({ open, onClose }) {
               enterTo='opacity-100 translate-y-0 sm:scale-100'
               leave='ease-in duration-200'
               leaveFrom='opacity-100 translate-y-0 sm:scale-100'
-              leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
-            >
+              leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'>
               <Dialog.Panel className='relative transform overflow-hidden rounded-xl bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6'>
                 {/* Content */}
                 <div className='mt-1 text-center'>
@@ -393,8 +417,7 @@ function PricingModal({ open, onClose }) {
                   <button
                     type='button'
                     className='inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-                    onClick={onClose}
-                  >
+                    onClick={onClose}>
                     Close
                   </button>
                 </div>
@@ -410,12 +433,12 @@ function PricingModal({ open, onClose }) {
 function PricingCalc() {
   let [tierIdx, setTierIdx] = useState(4);
 
-  let tier = tiers[tierIdx];
+  let tier = TIERS[tierIdx];
 
-  let price = Math.ceil(1620 * tier.unit) / 100;
+  let price = Math.ceil(PRICE_PER_CORE * tier.unit) / 100;
 
-  let ram = Math.floor(unitCore.ram * tier.unit);
-  let bandwidth = Math.floor(unitCore.bandwidth * tier.unit);
+  let ram = Math.floor(UNIT_CORE.ram * tier.unit);
+  let bandwidth = Math.floor(UNIT_CORE.bandwidth * tier.unit);
   let stats = [
     ['CPU Cores', `${tier.name} core`],
     ['RAM', `${ram} MB`],
@@ -460,27 +483,12 @@ function PricingCalc() {
   );
 }
 
-let unitCore = {
-  ram: 2000,
-  bandwidth: 1000
-};
-
-let tiers = [
-  { name: '¹⁄₁₆', unit: 1 / 16 },
-  { name: '⅛', unit: 1 / 8 },
-  { name: '¼', unit: 1 / 4 },
-  { name: '½', unit: 1 / 2 },
-  { name: '1', unit: 1 },
-  { name: '2', unit: 2 },
-  { name: '4', unit: 4 }
-];
-
 function PricingTabs({ tierIdx, setTierIdx }) {
   return (
     <div>
-      <div className='font-lg font-bold'>Number of CPU cores</div>
+      <div className='font-lg font-bold'>Number of CPU cores per server</div>
       <div className='isolate mt-2 flex flex w-full rounded-md shadow-sm'>
-        {tiers.map((tier, i) => {
+        {TIERS.map((tier, i) => {
           let current = i == tierIdx;
           return (
             <div
@@ -491,8 +499,7 @@ function PricingTabs({ tierIdx, setTierIdx }) {
                   ? 'z-10 bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
                   : 'px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0'
               )}
-              onClick={() => setTierIdx(i)}
-            >
+              onClick={() => setTierIdx(i)}>
               {tier.name}
             </div>
           );
