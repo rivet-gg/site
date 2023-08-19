d720bdfba4bf54806d4c7aa8b9625d2018c0f9b9
Nathan Flurry <git@nathanflurry.com>
2023-08-19 08:37:10 -0700

    Add colors to enginees

--- scripts/generateNavigation.mjs
+++ scripts/generateNavigation.mjs
@@ -198,29 +198,34 @@ function learnTabs(path) {
     {
       title: 'Unity',
       href: '/learn/unity',
+      gradient: ['from-cyan-300', 'to-cyan-500'],
       current: path[1] === 'unity'
     },
     {
       title: 'Unreal Engine',
       href: '/learn/unreal',
+      gradient: ['from-rose-400', 'to-rose-600'],
       current: path[1] === 'unreal'
     },
     {
       title: 'Godot',
       // icon: 'godot',
       href: '/learn/godot',
+      gradient: ['from-blue-300', 'to-blue-500'],
       current: path[1] === 'godot'
     },
     {
       title: 'HTML5',
       // icon: 'html5',
       href: '/learn/html5',
+      gradient: ['from-orange-400', 'to-orange-600'],
       current: path[1] === 'html5'
     },
     {
       title: 'Custom',
       // icon: 'docker',
       href: '/learn/custom',
+      gradient: ['from-slate-300', 'to-slate-500'],
       current: path[1] === 'custom'
     }
   ];
