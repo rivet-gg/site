ec5bce72da82f1db99264dfcf1c1786794dc2b29
Nathan Flurry <developer@nathanflurry.com>
2023-08-30 11:03:01 -0700

    Fix apostrophe

--- src/pages/oss-friends.jsx
+++ src/pages/oss-friends.jsx
@@ -4,16 +4,20 @@ import { Button } from '@/components/Button';
 export default function OssFriends({ items }) {
   return (
     <>
-      <h1>Rivet's <span className='text-purple-300'>Open Source</span> Friends</h1>
+      <h1>
+        {"Rivet's"} <span className='text-purple-300'>Open Source</span> Friends
+      </h1>
       <p>Other companies whose code & culture mirrors that at Rivet.</p>
 
       <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
         {items.map((friend, index) => (
-          <div key={index} className='flex flex-col overflow-hidden rounded-2xl bg-zinc-800 p-6 shadow-md outline outline-white/20'>
-            <a href={friend.href} className='mb-2 text-xl font-bold font-display'>
+          <div
+            key={index}
+            className='flex flex-col overflow-hidden rounded-2xl bg-zinc-800 p-6 shadow-md outline outline-white/20'>
+            <a href={friend.href} className='mb-2 font-display text-xl font-bold'>
               {friend.name}
             </a>
-            <p className='mt-0 text-sm text-white-800'>{friend.description}</p>
+            <p className='text-white-800 mt-0 text-sm'>{friend.description}</p>
             <div className='flex-grow' />
             <div className='mt-2'>
               <Button target='_blank' variant='primary' href={friend.href}>
