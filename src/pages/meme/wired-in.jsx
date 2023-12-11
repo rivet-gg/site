81d1b9d9e49c2729da2a3c51849650db4f7f916c
Nathan Flurry <git@nathanflurry.com>
2023-12-11 00:29:47 +0000

    fix comp

--- src/pages/meme/wired-in.jsx
+++ src/pages/meme/wired-in.jsx
@@ -4,7 +4,7 @@ export default function Index() {
 
         <div className="flex">
             <div className="w-[50%] flex items-center justify-center w-15 p-2 bg-red-900">
-                i'm wired in
+                {`i'm wired in`}
             </div>
             <iframe width="50%" height="500" src="https://www.youtube-nocookie.com/embed/PRBSKaTDrqQ?si=5jlDUh7aXIYev7Ty&amp;controls=0" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
