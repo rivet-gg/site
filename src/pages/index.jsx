a844bc717e32ce3877cc725ed39763d946ec90c2
Nicholas Kissel <Nicholas.kissel@icloud.com>
2023-12-22 21:17:46 -0800

    Color change for 'minutes'

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -892,7 +892,7 @@ function UpAndRunning() {
           {/* Body */}
           <div className='w-full flex-auto'>
             <h2 className='text-3xl font-bold tracking-tight text-white sm:text-4xl'>
-              Launch your game in <span className='text-violet-300'>minutes</span>
+              Launch your game in <span className='text-orange-400'>minutes</span>
             </h2>
             <p className='mt-6 text-lg leading-8 text-gray-300'>
               {`Just 5 lines of code and you're off to the races.`}
