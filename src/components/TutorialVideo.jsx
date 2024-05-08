b5d77c32ee61c7b9c235d1d5892a1943e3cbd2b3
NathanFlurry <git@nathanflurry.com>
2024-05-08 21:17:47 +0000

    fix: add back videos for godot tutorials (#185)

--- src/components/TutorialVideo.jsx
+++ src/components/TutorialVideo.jsx
@@ -1,9 +1,9 @@
-export function TutorialVideo({ src }) {
+export function TutorialVideo({ videoId }) {
   return (
     <iframe
       width='560'
       height='315'
-      src={src}
+      src={`https://www.youtube-nocookie.com/embed/${videoId}`}
       title='YouTube video player'
       frameBorder='0'
       allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
