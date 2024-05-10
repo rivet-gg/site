cc77b3e736fbd3123d673e53a3ce2d521283e008
Nicholas Kissel <Nicholas.kissel@icloud.com>
2024-05-10 14:55:24 -0700

    Orange Outline on Hover

--- src/components/highlights.jsx
+++ src/components/highlights.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import { useRef } from 'react';
 import clsx from 'clsx';
 
-export function IncludedHighlights({title, image}) {
+export function IncludedHighlights({ title, image }) {
   return (
     <div className={clsx(
       'relative transition text-sm font-semibold text-cream-100 border-2 bg-charcole-950 border-cream-100/20',
@@ -26,7 +26,7 @@ export default function SectionHighlight() {
         "grid-cols-1 sm:grid-cols-2 md:grid-cols-2",
       )}>
         <IncludedHighlights title="Container runtime written specifically for games" />
-        {/* <IncludedHighlights image={globe} /> */}
+        <VideoHighlight className="w-64 h-64" />
         <IncludedHighlights title="Edge routing & CDN architecture optimized for game traffic" />
         <IncludedHighlights title="Rugged, multi-region architecture built to withstand datacenter failures" />
         <IncludedHighlights title="JIT hardware provisioning to absorb DDoS attacks and player spikes" />
@@ -34,5 +34,22 @@ export default function SectionHighlight() {
         <IncludedHighlights title="Well documented architecture & SBOM" />
       </div>
     </div>
+  );
+}
+
+function VideoHighlight({ style, ...props }) {
+  const videoRef = useRef(null);
+  return (
+    <video
+      autoPlay
+      loop
+      muted
+      playsInline
+      ref={videoRef}
+      style={{ width: '250px', height: '250px' }}
+      {...props}
+    >
+      <source src="https://assets.rivet.gg/effects/globe.webm" type="video/webm" />
+    </video>
   );
 }
