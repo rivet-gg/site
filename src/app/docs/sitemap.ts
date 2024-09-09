40e954dade6c0fe7fe2cde8060dd53b8c597374a
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2024-09-09 14:27:12 +0200

    feat: modules page

--- src/app/docs/sitemap.ts
+++ src/app/docs/sitemap.ts
@@ -1,106 +1,108 @@
-import { Sitemap } from '@/lib/sitemap';
+import { Sitemap } from "@/lib/sitemap";
 
 export const sitemap = [
   {
-    title: 'General',
-    href: '/docs/general',
+    title: "General",
+    href: "/docs/general",
     sidebar: [
-      { href: '/docs/general' },
-      { title: 'General', pages: [{ href: '/docs/general/libraries' }] },
+      { href: "/docs/general" },
+      { title: "General", pages: [{ href: "/docs/general/libraries" }] },
       {
-        title: 'Concepts',
+        title: "Concepts",
         pages: [
           {
-            href: '/docs/general/concepts/authoritative-vs-p2p'
-          }
-        ]
+            href: "/docs/general/concepts/authoritative-vs-p2p",
+          },
+        ],
       },
       {
-        title: 'FAQ',
+        title: "FAQ",
         pages: [
-          { href: '/docs/general/faq/who-is-rivet-built-for' },
-          { href: '/docs/general/faq/who-is-rivet-not-built-for' }
-        ]
-      }
-    ]
+          { href: "/docs/general/faq/who-is-rivet-built-for" },
+          { href: "/docs/general/faq/who-is-rivet-not-built-for" },
+        ],
+      },
+    ],
   },
   {
-    title: 'Godot',
-    href: '/docs/godot',
+    title: "Godot",
+    href: "/docs/godot",
     sidebar: [
-      { href: '/docs/godot' },
+      { href: "/docs/godot" },
       {
-        title: 'Concepts',
-        pages: [{ href: '/docs/godot/concepts/resources' }]
+        title: "Concepts",
+        pages: [{ href: "/docs/godot/concepts/resources" }],
       },
       {
-        title: 'Tutorials',
-        pages: [{ href: '/docs/godot/tutorials/crash-course' }, { href: '/docs/godot/tutorials/bomber-demo' }]
-      }
-    ]
+        title: "Tutorials",
+        pages: [{ href: "/docs/godot/tutorials/crash-course" }, {
+          href: "/docs/godot/tutorials/bomber-demo",
+        }],
+      },
+    ],
   },
   {
-    title: 'Unity',
-    href: '/docs/unity',
+    title: "Unity",
+    href: "/docs/unity",
     sidebar: [
-      { href: '/docs/unity' },
+      { href: "/docs/unity" },
       {
-        title: 'Tutorials',
-        pages: [{ href: '/docs/unity/tutorials/fishnet/crash-course' }]
-      }
-    ]
+        title: "Tutorials",
+        pages: [{ href: "/docs/unity/tutorials/fishnet/crash-course" }],
+      },
+    ],
   },
   {
-    title: 'Unreal',
-    href: '/docs/unreal',
+    title: "Unreal",
+    href: "/docs/unreal",
     sidebar: [
-      { href: '/docs/unreal' },
+      { href: "/docs/unreal" },
       {
-        title: 'Concepts',
+        title: "Concepts",
         pages: [
-          { href: '/docs/unreal/concepts/resources' },
-          { href: '/docs/unreal/concepts/build-engine-from-source' },
-          { href: '/docs/unreal/concepts/run-methods' },
-          { href: '/docs/unreal/concepts/useful-commands' }
-        ]
+          { href: "/docs/unreal/concepts/resources" },
+          { href: "/docs/unreal/concepts/build-engine-from-source" },
+          { href: "/docs/unreal/concepts/run-methods" },
+          { href: "/docs/unreal/concepts/useful-commands" },
+        ],
       },
       {
-        title: 'Troubleshooting',
+        title: "Troubleshooting",
         pages: [
-          { href: '/docs/unreal/troubleshooting/chmod-error' },
-          { href: '/docs/unreal/troubleshooting/empty-level' },
-          { href: '/docs/unreal/troubleshooting/port-7777-already-taken' },
-          { href: '/docs/unreal/troubleshooting/standalone-wrong-map' }
-        ]
+          { href: "/docs/unreal/troubleshooting/chmod-error" },
+          { href: "/docs/unreal/troubleshooting/empty-level" },
+          { href: "/docs/unreal/troubleshooting/port-7777-already-taken" },
+          { href: "/docs/unreal/troubleshooting/standalone-wrong-map" },
+        ],
       },
       {
-        title: '⏱️ Crash Course',
+        title: "⏱️ Crash Course",
         pages: [
-          { href: '/docs/unreal/tutorials/crash-course' },
-          { href: '/docs/unreal/tutorials/crash-course/10-setup-project' },
-          { href: '/docs/unreal/tutorials/crash-course/20-setup-rivet' },
-          { href: '/docs/unreal/tutorials/crash-course/30-build-entry' },
-          { href: '/docs/unreal/tutorials/crash-course/40-deploy-rivet' }
-        ]
-      }
-    ]
+          { href: "/docs/unreal/tutorials/crash-course" },
+          { href: "/docs/unreal/tutorials/crash-course/10-setup-project" },
+          { href: "/docs/unreal/tutorials/crash-course/20-setup-rivet" },
+          { href: "/docs/unreal/tutorials/crash-course/30-build-entry" },
+          { href: "/docs/unreal/tutorials/crash-course/40-deploy-rivet" },
+        ],
+      },
+    ],
   },
   {
-    title: 'HTML5',
-    href: '/docs/html5',
+    title: "HTML5",
+    href: "/docs/html5",
     sidebar: [
-      { href: '/docs/html5' },
+      { href: "/docs/html5" },
       {
-        title: 'Tutorials',
+        title: "Tutorials",
         pages: [
-          { href: '/docs/html5/tutorials/crash-course' },
-          { href: '/docs/html5/tutorials/tanks-canvas-socketio' }
-        ]
-      }
-    ]
+          { href: "/docs/html5/tutorials/crash-course" },
+          { href: "/docs/html5/tutorials/tanks-canvas-socketio" },
+        ],
+      },
+    ],
   },
   {
-    title: 'Modules',
-    href: '/docs/modules'
-  }
+    title: "Modules",
+    href: "/docs/modules",
+  },
 ] satisfies Sitemap;
