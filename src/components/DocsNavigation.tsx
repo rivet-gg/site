31ab0e1ced1a721988c3fdecd0189283f6c592c4
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2024-09-10 00:08:24 +0200

    refactor: move docs to one directory for better navigation

--- src/components/DocsNavigation.tsx
+++ src/components/DocsNavigation.tsx
@@ -1,57 +1,139 @@
-'use client';
-import { usePathname } from 'next/navigation';
-import { sitemap } from '@/app/docs/sitemap';
-import routes from '@/generated/routes.json';
-import { ActiveSectionMarker } from '@/components/TableOfContents';
-import Link from 'next/link';
+"use client";
+import routes from "@/generated/routes.json";
+import { SidebarItem, SidebarSection } from "@/lib/sitemap";
+import { getAliasedHref } from "@/lib/sameAs";
+import { sitemap } from "@/app/docs/sitemap";
+import { ActiveSectionMarker } from "@/components/TableOfContents";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/sharp-solid-svg-icons";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
-export function DocsNavigation() {
-  const pathname = usePathname() || '';
+interface TreeItemProps {
+  item: SidebarItem;
+}
 
-  const currentPage = sitemap.find(page => pathname.startsWith(page.href));
+function TreeItem({ item }: TreeItemProps) {
+  if (
+    "collapsible" in item && "title" in item && "pages" in item &&
+    item.collapsible
+  ) {
+    return <AnimatedTreeItem item={item} />;
+  }
 
-  if (!currentPage || !currentPage.sidebar) return null;
+  if ("title" in item && "pages" in item) {
+    return (
+      <div className="group group-has-[.group]:pl-2">
+        <span className="font-sans text-xs font-semibold text-white">
+          {item.title}
+        </span>
+        <Tree
+          pages={item.pages}
+          className="has-[.group]:border-l has-[.group]:border-white/10"
+        />
+      </div>
+    );
+  }
 
   return (
-    <ul role='list' className='sticky top-docs-navigation text-white xl:pt-8'>
-      {currentPage.sidebar.map((item, index) => (
-        <li key={index} className='relative mb-4'>
-          {'title' in item ? (
-            <a href={item.href} className='font-sans text-xs font-semibold text-white'>
-              {item.title}
-            </a>
-          ) : (
-            <NavLink href={item.href}>{routes.pages[item.href]?.title}</NavLink>
-          )}
-          {'pages' in item && item.pages && (
-            <ul className='ml-4 mt-1'>
-              {item.pages.map((page, index) => (
-                <li key={index} className='relative'>
-                  <div className='relative'>
-                    <NavLink href={page.href}>{routes.pages[page.href]?.title}</NavLink>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          )}
+    <NavLink href={item.href}>
+      {routes.pages[getAliasedHref(item.href)]?.title}
+    </NavLink>
+  );
+}
+
+interface TreeProps {
+  pages: SidebarItem[];
+  className?: string;
+}
+
+export function Tree({ pages, className }: TreeProps) {
+  return (
+    <ul role="list" className={className}>
+      {pages.map((item, index) => (
+        <li key={index} className="relative">
+          <TreeItem item={item} />
         </li>
       ))}
     </ul>
   );
 }
 
-function NavLink({ href, children }) {
-  const pathname = usePathname() || '';
+export function NavLink({ href, children }) {
+  const pathname = usePathname() || "";
   const isCurrent = pathname === href;
   return (
     <>
-      {isCurrent ? <ActiveSectionMarker prefix='fafa' /> : null}
+      {isCurrent ? <ActiveSectionMarker prefix="fafa" /> : null}
       <Link
         href={href}
-        aria-current={isCurrent ? 'page' : undefined}
-        className='flex justify-between gap-2 py-1 pl-3 text-sm text-charcole-400 transition hover:text-white aria-current-page:text-white'>
-        <span className='truncate'>{children}</span>
+        aria-current={isCurrent ? "page" : undefined}
+        className="flex justify-between gap-2 py-1 pl-3 text-sm text-charcole-400 transition hover:text-white aria-current-page:text-white"
+      >
+        <span className="truncate">{children}</span>
       </Link>
     </>
+  );
+}
+
+interface AnimatedTreeItemProps {
+  item: SidebarSection;
+}
+
+export function AnimatedTreeItem({ item }: AnimatedTreeItemProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="group group-has-[.group]:pl-2 w-full ">
+      <button
+        className="appearance-none font-sans text-xs font-semibold flex justify-between w-full text-white"
+        onClick={() => setIsOpen((open) => !open)}
+      >
+        {item.title}
+        <motion.span
+          initial={{ rotateZ: "-90deg" }}
+          animate={{ rotateZ: isOpen ? 0 : "-90deg" }}
+          className="mr-2 -ml-1.5 inline-block"
+        >
+          <FontAwesomeIcon
+            icon={faChevronDown}
+          />
+        </motion.span>
+      </button>
+      <motion.div
+        className="overflow-hidden"
+        initial={false}
+        variants={{
+          open: { height: "auto", opacity: 1 },
+          closed: { height: 0, opacity: 0 },
+        }}
+        animate={isOpen ? "open" : "closed"}
+        transition={{
+          opacity: isOpen ? { delay: 0.3 } : {},
+          height: !isOpen ? { delay: 0.3 } : {},
+          duration: 0.3,
+        }}
+      >
+        <Tree
+          pages={item.pages}
+          className="has-[.group]:border-l has-[.group]:border-white/10"
+        />
+      </motion.div>
+    </div>
+  );
+}
+
+export function DocsNavigation() {
+  const pathname = usePathname() || "";
+
+  const currentPage = sitemap.find((page) => pathname.startsWith(page.href));
+
+  if (!currentPage || !currentPage.sidebar) return null;
+
+  return (
+    <div className="sticky top-docs-navigation text-white xl:pt-8 xl:pb-4">
+      <Tree pages={currentPage.sidebar} />
+    </div>
   );
 }
