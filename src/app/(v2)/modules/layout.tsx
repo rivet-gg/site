d899df21abf2a46c42adb0cc101d6c0be221b75c
jog1t <39823706+jog1t@users.noreply.github.com>
2024-09-26 11:01:32 +0000

    feat: changelog api (#342)

--- src/app/(v2)/modules/layout.tsx
+++ src/app/(v2)/modules/layout.tsx
@@ -1,10 +1,13 @@
 import { EmbedDetector } from '@/components/EmbedDetector';
 import { Header } from '@/components/v2/Header';
+import { Suspense } from 'react';
 
 export default function Layout({ children }) {
   return (
     <>
-      <EmbedDetector />
+      <Suspense fallback={null}>
+        <EmbedDetector />
+      </Suspense>
       <Header active='modules' />
       <main className='mx-auto max-w-6xl px-8'>{children}</main>
     </>
