54774c8c18641a91e926684562e638c34979aaee
jog1t <39823706+jog1t@users.noreply.github.com>
2024-09-26 11:01:31 +0000

    feat: hide header and footer when embedded (#341)

--- src/app/(v2)/modules/layout.tsx
+++ src/app/(v2)/modules/layout.tsx
@@ -1,8 +1,10 @@
+import { EmbedDetector } from '@/components/EmbedDetector';
 import { Header } from '@/components/v2/Header';
 
 export default function Layout({ children }) {
   return (
     <>
+      <EmbedDetector />
       <Header active='modules' />
       <main className='mx-auto max-w-6xl px-8'>{children}</main>
     </>
